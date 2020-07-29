47a144878e55554588f993fe5563f79c79da9be0
Kapil Ahuja <kapil99a@gmail.com>
2020-07-29 20:50:32 +0530

    add version file

--- tree-env/setup.py
+++ tree-env/setup.py
@@ -1,3 +1,5 @@
 from setuptools import setup
 
-setup(name='tree_env', version='0.0.1', install_requires=['gym'])
+exec(open('version.py').read())
+
+setup(name='tree_env', version=__version__, install_requires=['gym'])
